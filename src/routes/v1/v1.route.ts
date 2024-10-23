9b4b4344aa4c940cc9e75d848aac040a8e204fe7
조용원 <auau3@kakao.com>
2024-10-23 16:59:11 +0900

    feature: auth api 생성

--- src/routes/v1/v1.route.ts
+++ src/routes/v1/v1.route.ts
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import * as auth from "./auth.route";
 
 export const router = Router();
 export const path = "/v1";
+
+router.use(auth.path, auth.router);
---
+++ src/services/auth.service.ts
@@ -0,0 +1,32 @@
+import { StatusCodes } from "http-status-codes";
+import User from "../models/user.model";
+import { ApiError } from "../utils/error";
+import * as Yup from "yup";
+import JwtService from "./jwt.service";
+import logger from "../utils/logger";
+
+export default class AuthService {
+  public async login(username: string, password: string) {
+    const schema = Yup.object().shape({
+      username: Yup.string().required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid({ username, password })))
+      throw new ApiError("Bad Request", StatusCodes.BAD_REQUEST);
+
+    const user = await User.findOne({ where: { username: username } });
+
+    if (!user) throw new ApiError("Bad Request", StatusCodes.BAD_REQUEST);
+
+    if (password !== user.dataValues.password)
+      throw new ApiError("Unauthorized", StatusCodes.UNAUTHORIZED);
+
+    const token = JwtService.sign({
+      username: user.dataValues.username,
+      role: user.dataValues.role,
+    });
+
+    return { token };
+  }
+}
